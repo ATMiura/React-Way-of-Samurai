236c3ca8bb4b1460d4f2547c2609c917e25d6841
ATMiura <taske.dev.web@gmail.com>
2021-08-30 15:55:59 +0300

    Lesson 52 - React Side Effect

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,16 +4,20 @@ import * as axios from "axios";
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items);
-            });
-    }
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
+    };
+
 
     return (
         <div className={s.users}>
             <div className={s.usersItems}>
+                <button onClick={getUsers}>Get Users</button>
                 {
                     props.users.map(u => <div key={u.id} className={s.user + ' ' + s.active}>
                             <div className={s.users__avatar}>
