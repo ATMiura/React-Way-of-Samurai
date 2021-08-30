495c980c38ceb2196ebab77a10f5de3121eebe3c
ATMiura <taske.dev.web@gmail.com>
2021-08-30 15:40:58 +0300

    Lesson 51 - React Users API

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import s from './User.module.css';
+import * as axios from "axios";
 
 const Users = (props) => {
 
-    if(props.users.length === 0){
-        props.setUsers([
-            {id: 1, followed: false, fullName: 'Dmitriy',    status: 'I am a boss', avatar: 'https://i.pravatar.cc/270', location: { country: 'Belarus', city: 'Minsk',}},
-            {id: 2, followed: true,  fullName: 'Sasha',      status: 'I am a boss', avatar: 'https://i.pravatar.cc/270', location: { country: 'Russia', city: 'Moscow',}},
-            {id: 3, followed: false, fullName: 'Andrew',     status: 'I am a boss', avatar: 'https://i.pravatar.cc/270', location: { country: 'Ukraine', city: 'Kiev',}},
-            {id: 4, followed: true,  fullName: 'User name',  status: 'I am a boss', avatar: 'https://i.pravatar.cc/270', location: { country: 'User country', city: 'User city',}}
-        ]);
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
     return (
@@ -19,7 +18,7 @@ const Users = (props) => {
                     props.users.map(u => <div key={u.id} className={s.user + ' ' + s.active}>
                             <div className={s.users__avatar}>
                                 <picture className={s.users__picture}>
-                                    <img src={u.avatar} alt="" className={s.users__image}/>
+                                    <img src={u.photos.small != null ? u.photos.small : 'https://i.pravatar.cc/270' } alt="" className={s.users__image}/>
                                 </picture>
                                 { u.followed
                                     ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
@@ -27,12 +26,12 @@ const Users = (props) => {
                                 }
                             </div>
                             <div>
-                                <div className={s.users__fullName}>{u.fullName}</div>
+                                <div className={s.users__fullName}>{u.name}</div>
                                 <div className={s.users__status}>{u.status}</div>
                             </div>
                             <div className={s.location}>
-                                <div className={s.location__country}>{u.location.country}</div>
-                                <div className={s.location__city}>{u.location.city}</div>
+                                <div className={s.location__country}>{/*{u.location.country}*/}</div>
+                                <div className={s.location__city}>{/*{u.location.city}*/}</div>
                             </div>
                         </div>
                     )
