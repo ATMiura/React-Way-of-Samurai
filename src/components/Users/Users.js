3c0e109412979661abd85fc313ecf6b185e269d7
ATMiura <taske.dev.web@gmail.com>
2021-09-01 16:50:28 +0300

    Lesson 55 - React Pagination

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,23 +6,56 @@ class Users extends React.Component{
 
     getUsers = () => {
         if (this.props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
                 .then(response => {
                     this.props.setUsers(response.data.items);
+                    this.props.setTotalUsersCount(response.data.totalCount);
                 });
         }
     };
 
     componentDidMount() {
-        /* All side effects */
+        /* вызывается после первого рендеринга всего один раз All side effects */
         this.getUsers();
+    }
+
+    showMore() {
+
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
     }
 
     render() {
         /* Метод render обязательно вегда есть в классовой комопоненте */
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for(let i=1; i <pagesCount; i++){
+            pages.push(i);
+        }
+
         return (
             <div className={s.users}>
                 <div className={s.usersItems}>
+                    <div className={s.pagination}>
+                        <ul className={s.pagination__list}>
+                            {pages.map(p=>{
+                                return <li className={`${s.pagination__item} ${this.props.currentPage === p && s.active}`} onClick={(e)=>{ this.onPageChanged(p) }}>{p}</li>
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className={s.show__more}>
+                        <button onClick={this.showMore}>Show more</button>
+                    </div>
+
                     {
                         this.props.users.map(u => <div key={u.id} className={s.user + ' ' + s.active}>
                                 <div className={s.users__avatar}>
