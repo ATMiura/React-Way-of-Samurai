b9e92253a150eea69ae0f1a2f0c4360c3d962b9a
ATMiura <taske.dev.web@gmail.com>
2021-09-07 16:11:27 +0300

    Friends try

--- src/components/Navbar/FriendsContainer.jsx
+++ src/components/Navbar/FriendsContainer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import {connect} from "react-redux";
 import Friends from "./Friends";
-import {setFriends, toggleIsFetching} from "../../redux/sidebar-reducer";
+import {setFriends, toggleIsFetching, setTotalUsersCount} from "../../redux/sidebar-reducer";
 import Preloader from "../Common/Preloader/Preloader";
-import * as axios from "axios";
 import {usersAPI} from "../../api/api";
 
 class FriendsContainer extends React.Component{
 
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        usersAPI.getUsers().then(data => {
+        usersAPI.getUsers(null, this.props.totalCount).then(data => {
                 this.props.toggleIsFetching(false);
                 this.props.setFriends(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
             });
     }
 
@@ -30,7 +30,8 @@ const mapStateToProps = (state) => {
     return{
         sidebar: state.sidebar,
         friends: state.usersPage.users,
+        totalUsersCount: state.usersPage.totalUsersCount,
     }
 };
 
-export default connect(mapStateToProps, {setFriends, toggleIsFetching})(FriendsContainer);
+export default connect(mapStateToProps, {setFriends, toggleIsFetching, setTotalUsersCount})(FriendsContainer);
--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -1,9 +1,11 @@
 const FRIENDS_LIST = 'FRIENDS_LIST';
 const TOGGLE_IS_FETCHING = 'IS_FETCHING';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     friends: [],
-    isFetching: false
+    isFetching: false,
+    totalUsersCount: 0,
 };
 
 const sidebarReducer = (state = initialState, action) => {
@@ -13,6 +15,9 @@ const sidebarReducer = (state = initialState, action) => {
         case TOGGLE_IS_FETCHING: {
             return {...state, isFetching: action.isFetching}
         }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, totalUsersCount: action.count}
+        }
         default:
             return state;
     }
@@ -20,5 +25,6 @@ const sidebarReducer = (state = initialState, action) => {
 
 export const setFriends = (friends) => ({ type: FRIENDS_LIST, friends });
 export const toggleIsFetching = (isFetching) => ({ type: TOGGLE_IS_FETCHING, isFetching });
+export const setTotalUsersCount = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount });
 
 export default  sidebarReducer;
