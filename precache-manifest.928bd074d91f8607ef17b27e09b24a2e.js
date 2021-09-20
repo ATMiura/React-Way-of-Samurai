1fd038356c37a41cefb789005f25a8e93b43c31e
ATMiura <taske.dev.web@gmail.com>
2021-09-20 13:39:46 +0300

    Updates

---
+++ precache-manifest.928bd074d91f8607ef17b27e09b24a2e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5794f17b43becb5274d7",
+    "url": "/React-Way-of-Samurai/static/css/main.c39d3e3c.chunk.css"
+  },
+  {
+    "revision": "5794f17b43becb5274d7",
+    "url": "/React-Way-of-Samurai/static/js/main.5794f17b.chunk.js"
+  },
+  {
+    "revision": "2177eb0400d4d30d1ac1",
+    "url": "/React-Way-of-Samurai/static/css/1.938eb247.chunk.css"
+  },
+  {
+    "revision": "2177eb0400d4d30d1ac1",
+    "url": "/React-Way-of-Samurai/static/js/1.2177eb04.chunk.js"
+  },
+  {
+    "revision": "86107a40677c1b864066",
+    "url": "/React-Way-of-Samurai/static/css/2.b0f41cd8.chunk.css"
+  },
+  {
+    "revision": "86107a40677c1b864066",
+    "url": "/React-Way-of-Samurai/static/js/2.86107a40.chunk.js"
+  },
+  {
+    "revision": "7c76210b32152a1aa14a",
+    "url": "/React-Way-of-Samurai/static/js/3.7c76210b.chunk.js"
+  },
+  {
+    "revision": "db32f985bff6b9e85605",
+    "url": "/React-Way-of-Samurai/static/js/runtime~main.db32f985.js"
+  },
+  {
+    "revision": "29028579971da4762ebcbd1fdf9e4ee1",
+    "url": "/React-Way-of-Samurai/static/media/preloader.29028579.svg"
+  },
+  {
+    "revision": "17396e80dfa2eb79796c7ecacea9fbe9",
+    "url": "/React-Way-of-Samurai/index.html"
+  }
+];
